58f084552b4becb3019d4fb001955ed6e07d6b87
Richard Zvonek <zvonek.r@seznam.cz>
2021-05-15 15:45:49 +0200

    Renaming

---
+++ cleanup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Cleanup compiled files
+
+rm creationdate.lua
+rm creationdate.timestamp
+rm main.bcf
+rm pdfa.xmpi
+rm main.pdf
+rm main.aux
+rm main.fdb_latexmk
+rm main.lot
+rm main.fls
+rm main.log
+rm main.out
+rm main.run.xml
+rm main.toc
+rm main.bbl
+rm main.blg
+rm main.lof
+rm main.synctex.gz
+rm main.xdv
+rm main.pyg
+rm ./_minted-main -rf
